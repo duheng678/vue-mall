23d0f89673843e1533911c70170912577751de9e
Dada <duheng678@163.com>
2021-07-06 09:03:52 +0800

    vue-mall

---
+++ src/components/common/tabBar/TabBarItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tab-bar-item">
+    <div v-if="isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="active-item-icon"></slot></div>
+    <div><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem"
+};
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  height: 49px;
+  background-color: red;
+}
+.tab-bar-item img {
+  height: 24px;
+  width: 24px;
+}
+</style>
---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <tab-bar>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="" />
+      <img
+        slot="active-item-icon"
+        src="~assets/img/tabbar/home_active.svg"
+        alt=""
+      />
+
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item></tab-bar-item>
+    <tab-bar-item></tab-bar-item>
+    <tab-bar-item></tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "components/common/tabBar/TabBar";
+import TabBarItem from "components/common/tabBar/TabBarItem";
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+};
+</script>
+
+<style></style>
