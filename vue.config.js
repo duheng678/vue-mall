d713509b788eaeda3dd277b0cf297125dd80a70b
Dada <duheng678@163.com>
2021-07-11 16:23:35 +0800

    vue-mall

--- vue.config.js
+++ vue.config.js
@@ -14,5 +14,14 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     host: "0.0.0.0"
+  },
+  build: {
+    // Template for index.html
+    index: path.resolve(__dirname, "../docs/index.html"), //之前是'../dist/index.html'
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, "../docs"), // 之前是 '../dist'
+    assetsSubDirectory: "static",
+    assetsPublicPath: "./" // 之前是 '/'
   }
 };
