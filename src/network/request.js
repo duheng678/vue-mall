e8dd41b12217570da41464236b205425711a2154
Dada <duheng678@163.com>
2021-07-11 15:54:34 +0800

    vue-mall

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: "http://", //为了不暴露端口，这里不展示
+    baseURL: "http://152.136.185.210:7878/api/m5",
     timeout: 3000
   });
   instance.interceptors.request.use(
